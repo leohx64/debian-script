6ccbe24941f125636ad8c5e7e1f6ceaf616e0fe0
leohX64 <57879859+leohx64@users.noreply.github.com>
2024-12-13 18:54:11 -0300

    Add files via upload

---
+++ debian.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+sudo apt -y autoremove --purge libreoffice*
+
+sudo apt-get -y autoremove gnome-maps
+sudo apt-get -y autoremove gnome-clocks
+sudo apt-get -y autoremove gnome-weather
+sudo apt-get -y autoremove gnome-music
+sudo apt-get -y autoremove gnome-contacts
+sudo apt-get -y autoremove gnome-sound-recorder
+sudo apt-get -y autoremove gnome-clocks
+sudo apt-get -y autoremove rhythmbox
+sudo apt-get -y autoremove shotwell
+sudo apt-get -y autoremove synaptic
+sudo apt-get -y autoremove totem
+sudo apt-get -y autoremove evolution
+sudo apt-get -y autoremove simple-scan
+
+sudo apt-get -y autoremove gnome-sudoku
+sudo apt-get -y autoremove gnome-klotski
+sudo apt-get -y autoremove gnome-mines
+sudo apt-get -y autoremove gnome-chess
+sudo apt-get -y autoremove gnome-robots
+sudo apt-get -y autoremove gnome-taquin
+sudo apt-get -y autoremove gnome-2048
+sudo apt-get -y autoremove gnome-tetravex
+sudo apt-get -y autoremove gnome-mahjongg
+sudo apt-get -y autoremove gnome-nibbles
+sudo apt-get -y autoremove quadrapassel
+sudo apt-get -y autoremove aisleriot
+sudo apt-get -y autoremove swell-foop
+sudo apt-get -y autoremove four-in-a-row
+sudo apt-get -y autoremove lightsoff
+sudo apt-get -y autoremove iagno
+sudo apt-get -y autoremove tali
+sudo apt-get -y autoremove five-or-more
+sudo apt-get -y autoremove hitori
+sudo apt-get -y autoremove cheese
+
+
+
+sudo apt-get -y install git 
+sudo apt-get -y install curl
+sudo apt-get -y install build-essential
+sudo apt-get -y install python3-venv
+sudo apt-get -y install vlc
+sudo apt-get -y install drawing
+sudo apt-get -y install gnome-connections
+sudo apt-get -y install gnome-boxes
+
